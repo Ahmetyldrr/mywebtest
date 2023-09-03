375576295609de1095bca91b8e5040cdb244e61e
Ahmetyldrr <60220232+Ahmetyldrr@users.noreply.github.com>
2023-09-03 14:49:21 +0000

    /server ekelndi

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
 app = Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.H4('Interactive scatter plot with Iris dataset'),
